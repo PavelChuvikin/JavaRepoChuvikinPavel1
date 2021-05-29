02d15711dbd263fb5aed53913f73afea8b41f5bd
pavel_chuvikin <pavel_chuvikin@mail.ru>
2021-05-29 15:30:02 +0300

    homework3

--- homework3/Apple.java
+++
@@ -1,8 +0,0 @@
-public class Apple extends Fruit{
-    double weight;
-    Apple(){
-      type = "Apple";
-      weight = 1.0;
-
-    }
-}
--- homework3/Orange.java
+++
@@ -1,7 +0,0 @@
-public class Orange extends Fruit {
-    double weight;
-    Orange(){
-        type = "Orange";
-        weight = 1.5;
-    }
-}
